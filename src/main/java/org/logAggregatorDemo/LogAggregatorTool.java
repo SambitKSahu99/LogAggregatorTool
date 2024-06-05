4974faa70165ae4944206aab5cee0e58e2b1a069
Sambit <sambitkusahu.99@gmail.com>
2024-06-05 18:16:21 +0530

    Added Logic to accept user argument

--- src/main/java/org/logAggregatorDemo/LogAggregatorTool.java
+++ src/main/java/org/logAggregatorDemo/LogAggregatorTool.java
@@ -5,7 +5,7 @@ import java.io.File;
 public class LogAggregatorTool {
     public static void main(String[] args) {
 
-//      Taking input user through command line
+//      Taking user input through command line
         String folderPath = args[0];
         System.out.println(new File(folderPath).getName());
 
